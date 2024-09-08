6018f1c023ea7536eab6e933e30a8e9f928bb431
CodeSentinel-x <dawid.fratczak@proton.me>
2024-09-08 19:26:34 +0200

    test

--- gitPusher.sh
+++ gitPusher.sh
@@ -4,9 +4,15 @@ if [ -z "$1" ]; then
     echo -e "\033[31mNo commit message provided.\033[0m"
     exit 1
 fi
+    git add --all
+if [ -z "$2" ]; then
+    echo -e "\033[31mNo commit description provided.\033[0m"
+    git commit -m "$1"
+fi
+else
+    git commit -m "$1" "$2"
+end else
 
-git add --all
-git commit -m "$1"
 git push -u origin master               #This addition also clear all outpur from git push:     > /dev/null 2>&1 
 
 repoUrl=$(git config --get remote.origin.url)
